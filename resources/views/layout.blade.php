e967f6861e0d4ee71d1a811903ced3d0d04f43d1
tibidev7 <pricopgeorgetiberiu7@gmail.com>
2021-04-25 15:36:58 +0300

    Secure asset

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
         <script src="{{ mix('js/app.js') }}" defer></script>
         <title>{{ env('APP_NAME') }}</title>
     </head>
